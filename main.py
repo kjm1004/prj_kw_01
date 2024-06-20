c25f146da61a2b15924ca1a3af6733c6b9dd66dc
username <jokbal2000@gmail.>
2024-06-20 10:59:50 +0900

    1.1

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ from PyQt5.QtWidgets import QApplication
 from api.Kiwoom import *
 import sys
 
-from api.Kiwoom import Kiwoom2
+from api.Kiwoom import Kiwoom
 
 # app = QApplication(sys.argv)          <== 이벤트 루프 : OS의 이벤트, PyQt의 이벤트를 받아들임
 #   window = MyWindow()
 #   window.show()
 # app.exec_()     # main event loop     <== 루프 실행
 
-# app = QApplication(sys.argv)
-#
-# kiwoom = Kiwoom()
-# df = kiwoom.get_price_data("005930")
-# print(df)
-#
-# app.exec_()
+app = QApplication(sys.argv)
+
+kiwoom = Kiwoom()
+df = kiwoom.get_price_data("005930")
+print(df)
+
+app.exec_()
 
 
