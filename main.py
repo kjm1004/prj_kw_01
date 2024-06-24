780a267bd6306543b0d580c2da93851becf92eb9
username <jokbal2000@gmail.>
2024-06-24 13:29:05 +0900

    1.1

--- main.py
+++ main.py
@@ -18,22 +18,22 @@ kiwoom = Kiwoom()
 #deposit = kiwoom.get_deposit()
 
 # 주식 주문
-order_result = kiwoom.send_order('send_buy_order', '1001', 1, '007700', 10, 15100, '00')
-print("주문결과 : %s" % order_result)
+# order_result = kiwoom.send_order('send_buy_order', '1001', 1, '007700', 100, 15090, '00')
+# print("주문결과 : %s" % order_result)
 
 # 주식 체결 결과 보기
 # orders = kiwoom.get_order()
 # print(orders)
 
 # 주식 체결 잔고 현황 보기
-# position = kiwoom.get_balance()
-# print(position)
+position = kiwoom.get_balance()
+print(position)
 
 # 주식 실시간 체결현황 보기
 # 예시: kiwoom.set_real_reg("1000", "", get_fid("장운영구분"), "0")
-# fids = get_fid("체결시간")
-# codes = '005930;007700;000660;'
-# kiwoom.set_real_reg("1000", codes, fids, "0")
+fids = get_fid("체결시간")
+codes = '005930;007700;000660;'
+kiwoom.set_real_reg("1000", codes, fids, "0")
 
 app.exec_()
 
